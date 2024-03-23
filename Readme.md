f89b396d7b53e0405b12e71234a38972166c8372
Max Klassen <maximiliankk@gmail.com>
2024-03-23 15:14:05 -0700

    Readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Game Picker Bot
+### v1.0
+
+![Game Picker Bot Logo](GamePickerBot.png)
--- gamepickerbot.py
+++ gamepickerbot.py
@@ -14,12 +14,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # A list of games to pick from
-games_list = ['Hunt Showdown', 'Helldivers', 'PUBG']
+# games_list = ['Hunt Showdown', 'Helldivers', 'PUBG']
 
+# read the games from the file
+with open("default_games_list.txt", "r") as file:
+    games_list = file.read().splitlines()
+
+# Event listener for when the bot is ready
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+# Command to pick a game from the default list
 @bot.command(name='pickgame', help='Picks a random game for the session')
 async def pick_game(ctx):
     print('picking a game!')
